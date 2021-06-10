5f6355a26b4578c23b50adf4f75717d3e7ad7b13
sinyusuk <7504shlee@naver.com>
2021-06-10 10:11:34 +0900

    .

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('hello');
+app.get('/api/host',(req,res)=>{
+    res.send({
+        host:'tester'
+    });
 })
 
 app.listen(port,()=>{
